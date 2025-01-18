926d45a42a14643a1c6741eec26c0e088c901f32
Abdul Moeez Shaikh <abdulmoeezshaikh@gmail.com>
2025-01-18 17:28:20 -0500

    Bind Flask app to dynamic port

--- app.py
+++ app.py
@@ -170,4 +170,5 @@ def download_calendar(course_name):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    #app.run(debug=True)
